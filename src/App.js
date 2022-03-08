06314bc00a97c2a7c4f599293354d0da8a90c4e1
Jonathan Donegan <jondon@Jonathans-MacBook-Pro.local>
2022-03-08 14:06:40 +0000

    displays a list of all the articles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home";
+import { AllArticles } from './components/All_Articles';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <p>Nc News</p>
+        <Routes>
+          {/* <Route path = "/" element={<Home />} /> */}
+          <Route path="/" element={<AllArticles />} /> 
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+
+
+export const Home = () => {
+    return (
+        <div>
+           <p>Welcome to Nc-News</p>
+           <br/>
+           <p>this will be user page/ log in sign up etc </p>
+        </div>
+    );
+};
